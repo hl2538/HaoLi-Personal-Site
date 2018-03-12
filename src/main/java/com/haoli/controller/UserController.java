9ae3141894ab005d6cb374164fbfaf06eadc3293
李昊 <lihao_100@boe.com.cn>
2018-03-12 17:28:52 +0800

    小修改

--- src/main/java/com/haoli/controller/UserController.java
+++ src/main/java/com/haoli/controller/UserController.java
@@ -3,12 +3,11 @@ package com.haoli.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.haoli.domain.User;
@@ -35,6 +34,7 @@ public class UserController {
 		userService.register(user);
 		return JsonResponse.success("register succeed");
 	}
+
 
 	
 }
